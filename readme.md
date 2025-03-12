cc807c0ed134b2f2c679709460a6318914f6a2f1
yuyang <leirong@papegames.net>
2025-03-12 19:53:12 +0800

    feat: 添加健康检查&定时请求

--- readme.md
+++ readme.md
@@ -2,11 +2,13 @@
 
 ## 项目介绍
 
-基于express实现王者荣耀英雄资料库服务端
+基于 express 实现王者荣耀英雄资料库服务端
 
 ## 接口
-* [查询英雄](https://wzry-database-server.onrender.com/heros)
-* [查询局内道具](https://wzry-database-server.onrender.com/items)
-* [查询召唤师技能](https://wzry-database-server.onrender.com/summoners)
-* [查询赛季列表](https://wzry-database-server.onrender.com/leagues)
-* [查询英雄数据排行](https://wzry-database-server.onrender.com/hero-data?league_id=20240001)
+
+- [健康检查](https://wzry-database-server.onrender.com/health)
+- [查询英雄](https://wzry-database-server.onrender.com/heros)
+- [查询局内道具](https://wzry-database-server.onrender.com/items)
+- [查询召唤师技能](https://wzry-database-server.onrender.com/summoners)
+- [查询赛季列表](https://wzry-database-server.onrender.com/leagues)
+- [查询英雄数据排行](https://wzry-database-server.onrender.com/hero-data?league_id=20240001)
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const axios = require('axios');
-const cors = require('cors');
+const axios = require('axios')
+const cors = require('cors')
 
 // set up express web server
 const app = express()
@@ -12,72 +12,81 @@ app.use(express.static('public'))
 const handleRequestError = (response, code, errorMessage) => {
   response.status(code).json({
     code,
-    errorMessage
-  });
-};
+    errorMessage,
+  })
+}
+
+app.get('/health', (_request, response) => {
+  response.json({
+    code: 0,
+    data: 'ok',
+  })
+})
 
 app.get('/heros', async (_request, response) => {
   try {
-    const res = await axios.get('https://pvp.qq.com/web201605/js/herolist.json');
+    const res = await axios.get('https://pvp.qq.com/web201605/js/herolist.json')
     response.json({
       code: 0,
-      data: res.data
-    });
+      data: res.data,
+    })
   } catch (error) {
-    handleRequestError(response, 500, '英雄查询失败');
+    handleRequestError(response, 500, '英雄查询失败')
   }
-});
+})
 
 app.get('/items', async (_request, response) => {
   try {
-    const res = await axios.get('https://pvp.qq.com/web201605/js/item.json');
+    const res = await axios.get('https://pvp.qq.com/web201605/js/item.json')
     response.json({
       code: 0,
-      data: res.data
-    });
+      data: res.data,
+    })
   } catch (error) {
-    handleRequestError(response, 500, '局内道具查询失败');
+    handleRequestError(response, 500, '局内道具查询失败')
   }
-});
+})
 
 app.get('/summoners', async (_request, response) => {
   try {
-    const res = await axios.get('https://pvp.qq.com/web201605/js/summoner.json');
+    const res = await axios.get('https://pvp.qq.com/web201605/js/summoner.json')
     response.json({
       code: 0,
-      data: res.data
-    });
+      data: res.data,
+    })
   } catch (error) {
-    handleRequestError(response, 500, '召唤师技能查询失败');
+    handleRequestError(response, 500, '召唤师技能查询失败')
   }
-});
+})
 
 app.get('/leagues', async (_request, response) => {
   try {
-    const res = await axios.get('https://prod.comp.smoba.qq.com/leaguesite/leagues/open');
+    const res = await axios.get('https://prod.comp.smoba.qq.com/leaguesite/leagues/open')
     response.json({
       code: 0,
-      data: res.data.results.reverse()
-    });
+      data: res.data.results.reverse(),
+    })
   } catch (error) {
-    handleRequestError(response, 500, '联赛查询失败');
+    handleRequestError(response, 500, '联赛查询失败')
   }
-});
+})
 
 app.get('/hero-data', async (_request, response) => {
   try {
     const { league_id } = _request.query
-    const res = await axios.get(`https://prod.comp.smoba.qq.com/leaguesite/league/hero/settle_list/open?league_id=${league_id}`);
+    const res = await axios.get(
+      `https://prod.comp.smoba.qq.com/leaguesite/league/hero/settle_list/open?league_id=${league_id}`,
+    )
     response.json({
       code: 0,
-      data: res.data.data
-    });
+      data: res.data.data,
+    })
   } catch (error) {
-    handleRequestError(response, 500, '英雄数据查询失败');
+    handleRequestError(response, 500, '英雄数据查询失败')
   }
-});
+})
 
 // Start web server on port 3001
 app.listen(3001, () => {
-  console.log(`Server is listening at http://localhost:3001`);
+  console.log(`Server is listening at http://localhost:3001`)
 })
